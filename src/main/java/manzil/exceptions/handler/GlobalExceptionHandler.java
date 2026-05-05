d188ec1ca3628e38780d7d6dad07e498c8ead323
bilal <biluthescorpional@gmail.com>
2026-05-05 18:57:17 +0500

    Finished Global Exception Handler

--- src/main/java/manzil/exceptions/handler/GlobalExceptionHandler.java
+++ src/main/java/manzil/exceptions/handler/GlobalExceptionHandler.java
@@ -1,5 +1,19 @@
 package manzil.exceptions.handler;
 
+import jakarta.annotation.Resource;
+import manzil.exceptions.ResourceNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
 public class GlobalExceptionHandler
 {
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<String> handleNotFound(ResourceNotFoundException e)
+    {
+        return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
 }
