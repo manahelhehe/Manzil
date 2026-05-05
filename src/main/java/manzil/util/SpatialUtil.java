b29571224906ca8816b7cbea746ff4bba011ae54
bilal <biluthescorpional@gmail.com>
2026-05-05 19:16:20 +0500

    Utility Package Added + SpatialUtil Set Up

---
+++ src/main/java/manzil/util/SpatialUtil.java
@@ -0,0 +1,18 @@
+package manzil.util;
+
+import org.locationtech.jts.geom.Coordinate;
+import org.locationtech.jts.geom.GeometryFactory;
+import org.locationtech.jts.geom.Point;
+import org.locationtech.jts.geom.PrecisionModel;
+
+public class SpatialUtil
+{
+    static GeometryFactory geoFactory = new GeometryFactory(new PrecisionModel(), 4326);
+
+    private SpatialUtil() {}
+
+    public static Point mapLocation(int lat, int lng)
+    {
+        return geoFactory.createPoint(new Coordinate(lat, lng));
+    }
+}
